23fe2d534ddb6b3d1f35a63efd93384d08990403
zy <zy@zy.com>
2025-06-26 19:32:38 +0800

    输出节点完善, 增加sub_path和sep属性

--- backend/processor/Output.py
+++ backend/processor/Output.py
@@ -17,6 +17,11 @@ class SaveNodeBase(metaclass=ProcessorMeta):
         self.prefix = ""
         self.suffix = ".dat"
         self.mode = "wb"  # 默认二进制模式
+        self.fd = None
+
+    def __del__(self):
+        if self.fd is not None:
+            self.fd.close()
 
     def load(self, xml_node):
         # 未配置保存节点使用默认节点是xml_node参数是None
@@ -25,8 +30,13 @@ class SaveNodeBase(metaclass=ProcessorMeta):
             return
 
         self.name = xml_node.attrib.get("name", self.package.name)
-        self.prefix = xml_node.attrib.get("prefix", "")
+        self.prefix = xml_node.attrib.get("prefix", self.prefix)
         self.suffix = xml_node.attrib.get("suffix", self.suffix)
+
+        self.filename = self.package.global_save_path / (self.prefix + self.name + self.suffix)
+        self.fd = open(self.filename, self.mode)
+        if self.fd is None:
+            raise RuntimeError(f"{self.package.name}-{self.name}: open save file error {self.filename}")
 
     @abstractmethod
     def pack(self, data, /, **kwargs) -> bool:
@@ -45,24 +55,13 @@ class DatSaveNode(SaveNodeBase):
         self.fd = None
         self.mode = "wb"  # 默认二进制模式
 
-    def __del__(self):
-        if self.fd is not None:
-            self.fd.close()
-
-    def load(self, xml_node):
-        super().load(xml_node)
-        self.filename = self.package.global_save_path / (self.prefix + self.name + self.suffix)
-        self.fd = open(self.filename, self.mode)
-        if self.fd is None:
-            raise RuntimeError(f"{self.package.name}-{self.name}: open save file error {self.filename}")
-
     def pack(self, data, /, **kwargs) -> bool:
         if self.fd is not None:
             self.fd.write(data)
         return True
 
 
-class TxtSaveNode(DatSaveNode):
+class TxtSaveNode(SaveNodeBase):
     """
     保存为txt文件
     """
@@ -72,14 +71,25 @@ class TxtSaveNode(DatSaveNode):
         self.suffix = ".txt"
         self.mode = "wt"
 
+    def __del__(self):
+        if self.fd is not None:
+            self.fd.close()
+
+    def load(self, xml_node):
+        super().load(xml_node)
+        self.sep = xml_node.attrib.get("sep", "")  # 分隔符，默认为空
+
     def pack(self, data, /, **kwargs) -> bool:
         if self.fd is not None:
-            self.fd.write(data.hex())
+            if len(self.sep) == 1:
+                self.fd.write(data.hex(self.sep))
+            else:
+                self.fd.write(data.hex())
             self.fd.write("\n")
         return True
 
 
-class SingleDatSaveNode(SaveNodeBase):
+class SingleDatSaveNode(DatSaveNode):
     """
     保存为单个dat文件
     """
@@ -92,7 +102,10 @@ class SingleDatSaveNode(SaveNodeBase):
     def load(self, xml_node):
         super().load(xml_node)
 
-        self.sub_path = self.package.global_save_path / self.name
+        self.sub_path = xml_node.attrib.get("sub_path", None)
+        if self.sub_path is None:
+            self.sub_path = self.name + "_dat"
+        self.sub_path = self.package.global_save_path / self.sub_path
         os.makedirs(self.sub_path, exist_ok=True)
 
     def pack(self, data, /, **kwargs) -> bool:
@@ -102,7 +115,7 @@ class SingleDatSaveNode(SaveNodeBase):
         return True
 
 
-class SingleTxtSaveNode(SingleDatSaveNode):
+class SingleTxtSaveNode(TxtSaveNode):
     """
     保存为单个txt文件
     """
@@ -112,9 +125,21 @@ class SingleTxtSaveNode(SingleDatSaveNode):
         self.suffix = ".txt"
         self.mode = "wt"  # 默认二进制模式
 
+    def load(self, xml_node):
+        super().load(xml_node)
+
+        self.sub_path = xml_node.attrib.get("sub_path", None)
+        if self.sub_path is None:
+            self.sub_path = self.name + "_txt"
+        self.sub_path = self.package.global_save_path / self.sub_path
+        os.makedirs(self.sub_path, exist_ok=True)
+
     def pack(self, data, /, **kwargs) -> bool:
         filename = self.sub_path / (self.prefix + self.name + f"_{self.package._cur_pkg:06}" + self.suffix)
         with open(filename, self.mode) as fd:
-            fd.write(data.hex())
+            if len(self.sep) == 1:
+                fd.write(data.hex(self.sep))
+            else:
+                fd.write(data.hex())
             fd.write("\n")
         return True
